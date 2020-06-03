7fc01e09331973f268e526e046ec274ec8ed98d1
Wentao Yang <wentao.yang@mail.utoronto.ca>
2020-06-02 23:48:36 -0700

    added a faster method for sorted_squares

--- hacker_rank/arrays/sorted_squares.py
+++ hacker_rank/arrays/sorted_squares.py
@@ -1,19 +1,29 @@
 from typing import *
 
 
-def sortedSquares(A: List[int]) -> List[int]:
+def sorted_squares(a: List[int]) -> List[int]:
     result = []
+    current_min = a[0]
+    current_max = 0
 
-    for i in range(len(A)):
-        squared = A[i] ** 2
-        if len(result) == 0:
-            result.append(squared)
+    for number in a:
+        number_squared = number * number
+        if number_squared >= current_max:
+            result.append(number_squared)
+            current_max = number_squared
+        elif number_squared <= current_min:
+            result.insert(0, number_squared)
+            current_min = number_squared
         else:
-            for j in range(len(result)):
-                if result[j] > squared:
-                    result.insert(j, squared)
+            inserted = False
+            for i in range(len(result)):
+                if result[i] > number_squared and not inserted:
+                    result.insert(i, number_squared)
+                    inserted = True
+            if not inserted:
+                result.append(number_squared)
     return result
 
 
 if __name__ == "__main__":
-    print(sortedSquares([-4, -1, 0, 3, 10]))
+    print(sorted_squares([-4, -1, 0, 3, 10]))
