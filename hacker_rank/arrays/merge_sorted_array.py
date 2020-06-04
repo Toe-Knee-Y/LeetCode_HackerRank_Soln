3badee6a8fd37aea9b045f357fdf9a6e09f701cd
Wentao Yang <wentao.yang@mail.utoronto.ca>
2020-06-03 23:12:45 -0700

    added comment to better understand the code

--- hacker_rank/arrays/merge_sorted_array.py
+++ hacker_rank/arrays/merge_sorted_array.py
@@ -5,6 +5,7 @@ def merge(nums1: List[int], m: int, nums2: List[int], n: int) -> None:
     """
     Do not return anything, modify nums1 in-place instead.
     """
+    # When nums 1 is empty, we just copy everything from nums2
     if m == 0:
         for k in range(n):
             nums1[k] = nums2[k]
@@ -13,12 +14,15 @@ def merge(nums1: List[int], m: int, nums2: List[int], n: int) -> None:
         j = 0
         while j != n:
             if nums2[j] <= nums1[i]:
+                # insert the element to nums1 and shift everything down ward
                 nums1.insert(i, nums2[j])
                 nums1.pop()
                 j += 1
                 i += 1
             else:
                 i += 1
+                # m + j implies that we have iterated all the original elements in nums1
+                # hence we need to add values after the original elements
                 if i == m + j:
                     nums1[i] = nums2[j]
                     j += 1
